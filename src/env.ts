d761d792e7fede4a0858ade4b168f89403ab7df4
eydrenn <endreyestudos67@gmail.com>
2025-08-10 17:32:58 -0300

    refactor: dotenv added

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
 import { validateEnv } from "#base";
 import { z } from "zod";
 
-export const env = validateEnv(z.object({
-    BOT_TOKEN: z.string("Discord Bot Token is required").min(1),
-    WEBHOOK_LOGS_URL: z.url().optional()
-}));
+export const env = validateEnv(
+	z.object({
+		BOT_TOKEN: z.string("Discord Bot Token is required").min(1),
+	}),
+);
